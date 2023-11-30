94a8446130041fceea0b13478d269f94080d3faa
riskautami18 <152425862+riskautami18@users.noreply.github.com>
2023-11-30 23:16:40 +0700

    Add files via upload

---
+++ rizka.php
@@ -0,0 +1,221 @@
+<?php include 'header.php';?>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img widht="300" height="300" class="rounded-circle"src="https://w7.pngwing.com/pngs/817/373/png-transparent-naturally-nutritious-bakery-logo.png" alt="">
+          <h1 class="display-4">Cheers Up Bakery</h1>
+          <p class="lead">Cheers Up Bakery adalah toko kue yang menyediakan berbagai jenis kue yang variatif dan lezatt.</p>
+        </div>
+      <div class="container text-center">
+
+        <h1>about</h1>
+      <div class="row">
+        <div class="col text-justify">
+            <p>Cheers Up Bakery adalah jenis usaha Bakery yang buka setiap hari 
+(Senin-Minggu) dan beroperasi mulai dari pukul 09.00-21.00, yang menyediakan 
+berbagai macam roti, kue, dan kue kering yang berbahan dasar rendah 
+lemak yang tentunya menyehatkan. </p>
+        </div>
+        <div class="col text-justify">
+            <p>Cheers Up Bakery didirikan karena pemilik melihat 
+semakin maraknya bisnis makanan dan peluang dari tren hidup sehat yang 
+sekarang sedang populer di kota-kota besar Indonesia khususnya di kota purwokero.</p>
+        </div>
+      </div>
+
+      <div class="card-deck m-3">
+        <div class="card">
+          <img src="https://i.pinimg.com/1200x/88/fe/06/88fe060c39c1963d6f0ef7fb91ee5ad9.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Birthday cake</h5>
+            <p class="card-text">Birthday cake merupakan kue yang disajikan saat perayaan hari ulang tahun. Biasanya kue ulang tahun berupa kue tart yang dihiasi dengan nama seseorang, pesan selamat, dan lilin yang dihidupkan untuk ditiup oleh yang akan berulang tahun.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://nibble-images.b-cdn.net/nibble/original_images/sejarah_croissant_6_e30b8c8ed3_Bt489iShY-.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Croissant</h5>
+            <p class="card-text">Croissant atau roti sabit adalah sejenis kue kering yang berasal dari Prancis. Dinamakan demikian karena bentuknya menyerupai bulan sabit. Dikutip dari Le dictionnaire Larousse, salah satu makna kata croissant adalah bulan sabit. Bahan dasar kue kering ini adalah mentega, air, tepung, ragi, gula, garam.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://kurio-img.kurioapps.com/21/08/12/001ad501-4b96-4d4d-bb0b-92d60855761b.jpe" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Cheesecake</h5>
+            <p class="card-text">Cheesecake (bahasa Indonesia: "kue keju") adalah kue yang biasanya dimakan sebagai hidangan penutup, dibuat dengan mencampurkan keju yang bertekstur lembut, telur, susu, dan gula.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-deck m-3">
+        <div class="card">
+          <img src="https://static01.nyt.com/images/2022/02/12/dining/JT-Chocolate-Chip-Cookies/JT-Chocolate-Chip-Cookies-master768.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Cookies</h5>
+            <p class="card-text">Cookies adalah makanan yang dipanggang atau dimasak yang biasanya kecil, datar, dan manis. Kukis biasanya terdiri dari tepung, gula, dan beberapa jenis minyak atau lemak. Kukis juga dapat dicampur dengan bahan-bahan lain seperti kismis, gandum, keping cokelat, kacang-kacangan, dll.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://kirin.co.id/wp-content/uploads/2022/01/macam-macam-kue-kering.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Kue Kering</h5>
+            <p class="card-text">Kue kering ialah camilan yang lumayan digemari oleh masyarakat dari anak kecil hingga dengan orang dewasa, diketahui bahwa kue kering memiliki rasa yang manis dan tekstur yang rapuh, kue kering pada umumnya mampu disimpan sampai waktu yang cukup lama. Selain itu, cara pembuatannya juga sangat mudah.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="https://moondoggiesmusic.com/wp-content/uploads/2019/03/untitled-f5bbecb08b2ee92b398ee3e9124efb2a.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Donat</h5>
+            <p class="card-text">Donat adalah penganan yang digoreng, dibuat dari adonan tepung terigu, gula pasir, kuning telur, ragi roti, dan mentega. Donat yang paling umum adalah donat dengan bentuk seperti cincin dan ada lubang di tengahnya, biasanya ditaburi dengan mesis atau gula halus.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+     
+      <?php include 'footer.php';?>
+       <!-- Footer -->
+ <footer class="text-center text-lg-start bg-light text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+  
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>Cheers Bakery
+            </h6>
+            <p>
+              Cheers Bakery menjadi perusahaan kue yang lezaat variatif dan terbaik di indonesia
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Products
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Birthday Cake</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Croissant</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">cheesecake</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Cookies</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Kue Kering </a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Donat</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Useful links
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Pricing</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Settings</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Orders</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Help</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+            <p><i class="fas fa-home me-3"></i>Indonesia,ID 523456, IDN</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              info@Cheers_Bakery
+            </p>
+            <p><i class="fas fa-phone me-3"></i> +6223456788</p>
+            <p><i class="fas fa-print me-3"></i> +6223456789</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2021 Copyright:
+      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
